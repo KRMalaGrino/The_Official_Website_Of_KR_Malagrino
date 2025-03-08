75448962438a6ea9fab2eabd4f375ae6aed0f704
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-03-08 14:03:10 -0800

    feat: javascript highlights array skeleton

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+// --------------------Highlights array --------------------------
+
+const highlights = [
+        {
+            name: "Look Up"
+            link: "https://www.youtube.com/embed/zbF3SsoTUvA?si=is5eNOwXuqZPKRKu"
+        }
+        {
+            name: "A Lil' Fancy"
+            link: "https://www.youtube.com/embed/iPqlSTPTNC8?si=xnzyXWf0mJZu5Vay"
+        }
+        {
+            name: "Kahina"
+            link: "https://www.youtube.com/embed/A0dovpf779A?si=uVPKRurhaxLZHkEe"
+        }
+        {
+            name: "Erth"
+            link: "https://www.youtube.com/embed/ZulWAyu0OlQ?si=-b7Xpdv1QamSfuoE"
+        }
+        {
+            name: "Mercury"
+            link: "https://www.youtube.com/embed/h7zJg8XM7Fs?si=kOjgmIZbLb9qIkkn"
+        }
+        {
+            name: "New World"
+            link: "https://www.youtube.com/embed/csjlAccd3aQ?si=JNixjpBn2WXMxkck"
+        }
+        {
+            name: "All She Wrote"
+            link: "https://www.youtube.com/embed/wahvaciH9Xg?si=OSRlq4IX13pio3fE"
+        }
+        {
+            name: "Save The Children"
+            link: "https://www.youtube.com/embed/S9YGRYt6E34?si=9-KZA2lEEQR-3j8m"
+        }
+        {
+            name: "C.O.R.E."
+            link: "#"
+        }
+    ];
+
+// --------------------Discography array --------------------------
