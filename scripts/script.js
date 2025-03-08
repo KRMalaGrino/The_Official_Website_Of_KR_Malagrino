828e7755ce1b0ff08f26073dab63f5766aa16f26
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-03-08 14:23:28 -0800

    feat: javascript discogrpahy array skeleton

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,95 @@
 // --------------------Highlights array --------------------------
 
 const highlights = [
-        {
-            name: "Look Up"
-            link: "https://www.youtube.com/embed/zbF3SsoTUvA?si=is5eNOwXuqZPKRKu"
-        }
-        {
-            name: "A Lil' Fancy"
-            link: "https://www.youtube.com/embed/iPqlSTPTNC8?si=xnzyXWf0mJZu5Vay"
-        }
-        {
-            name: "Kahina"
-            link: "https://www.youtube.com/embed/A0dovpf779A?si=uVPKRurhaxLZHkEe"
-        }
-        {
-            name: "Erth"
-            link: "https://www.youtube.com/embed/ZulWAyu0OlQ?si=-b7Xpdv1QamSfuoE"
-        }
-        {
-            name: "Mercury"
-            link: "https://www.youtube.com/embed/h7zJg8XM7Fs?si=kOjgmIZbLb9qIkkn"
-        }
-        {
-            name: "New World"
-            link: "https://www.youtube.com/embed/csjlAccd3aQ?si=JNixjpBn2WXMxkck"
-        }
-        {
-            name: "All She Wrote"
-            link: "https://www.youtube.com/embed/wahvaciH9Xg?si=OSRlq4IX13pio3fE"
-        }
-        {
-            name: "Save The Children"
-            link: "https://www.youtube.com/embed/S9YGRYt6E34?si=9-KZA2lEEQR-3j8m"
-        }
-        {
-            name: "C.O.R.E."
-            link: "#"
-        }
-    ];
+  {
+    song: "Look Up",
+    link: "https://www.youtube.com/embed/zbF3SsoTUvA?si=is5eNOwXuqZPKRKu",
+  },
+  {
+    song: "A Lil' Fancy",
+    link: "https://www.youtube.com/embed/iPqlSTPTNC8?si=xnzyXWf0mJZu5Vay",
+  },
+  {
+    song: "Kahina",
+    link: "https://www.youtube.com/embed/A0dovpf779A?si=uVPKRurhaxLZHkEe",
+  },
+  {
+    song: "Erth",
+    link: "https://www.youtube.com/embed/ZulWAyu0OlQ?si=-b7Xpdv1QamSfuoE",
+  },
+  {
+    song: "Mercury",
+    link: "https://www.youtube.com/embed/h7zJg8XM7Fs?si=kOjgmIZbLb9qIkkn",
+  },
+  {
+    song: "New World",
+    link: "https://www.youtube.com/embed/csjlAccd3aQ?si=JNixjpBn2WXMxkck",
+  },
+  {
+    song: "All She Wrote",
+    link: "https://www.youtube.com/embed/wahvaciH9Xg?si=OSRlq4IX13pio3fE",
+  },
+  {
+    song: "Save The Children",
+    link: "https://www.youtube.com/embed/S9YGRYt6E34?si=9-KZA2lEEQR-3j8m",
+  },
+  {
+    song: "C.O.R.E.",
+    link: "#",
+  },
+];
 
 // --------------------Discography array --------------------------
+
+const discography = [
+  {
+    record: "Octave .0",
+    type: "Album",
+    link: "https://open.spotify.com/embed/album/1j8F0KOLxKiKPHQMcBxPf1?utm_source=generator&theme=0",
+  },
+  {
+    record: "Look Up",
+    type: "Single",
+    link: "https://open.spotify.com/embed/album/5rUnTmZTv7VWrY64Ofofy7?utm_source=generator&theme=0",
+  },
+  {
+    record: "Filthy Rags",
+    type: "Single",
+    link: "https://open.spotify.com/embed/album/3bPW2pKTVc2U1thtpv07Mv?utm_source=generator&theme=0",
+  },
+  {
+    record: "SEED-3000",
+    type: "Album",
+    link: "https://open.spotify.com/embed/album/3hYcn73DOge96MiPc1PFKW?utm_source=generator&theme=0",
+  },
+  {
+    record: "A Lil' Fancy",
+    type: "Single",
+    link: "https://open.spotify.com/embed/album/3Irm7unt1ZNJAXNuHqKpSz?utm_source=generator&theme=0",
+  },
+  {
+    record: "L'OWL'Y",
+    type: "E.P.",
+    link: "https://open.spotify.com/embed/album/3iGx0LC84dpNiWC9ogkezp?utm_source=generator&theme=0",
+  },
+  {
+    record: "REALM IV",
+    type: "E.P.",
+    link: "https://open.spotify.com/embed/album/553ZbYATN2ZZeyUhVE7q5o?utm_source=generator&theme=0",
+  },
+  {
+    record: "S-O-L",
+    type: "E.P.",
+    link: "https://open.spotify.com/embed/album/7Bm3ub7N78GKzdH05w7jlW?utm_source=generator&theme=0",
+  },
+  {
+    record: "III DOOR",
+    type: "Album",
+    link: "https://open.spotify.com/embed/album/2sXPrZBVJwHbCjc3R87FCP?utm_source=generator&theme=0",
+  },
+  {
+    record: "T-PO'AK",
+    type: "Album",
+    link: "https://open.spotify.com/embed/album/17KtMScOLI3Ic8fIXUtksL?utm_source=generator&theme=0",
+  },
+];
