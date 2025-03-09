3a3d30db30e52e40ae0d4604d22578772ce62024
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-03-08 23:45:10 -0800

    feat:lyrics array added. 3 songs left for octave 0

---
+++ scripts/lyrics.js
@@ -0,0 +1,34 @@
+const Octave0Lyrics = [
+  {
+    title: "Da Bubu",
+    lyrics:
+      "Lately there's been alot of lovey-dovey - round 4 with the wifey like bunnies - bathe her in the water of the word - the water with the honey - she stays naked in the house - so pleasing to be in her presence - the king is back on the throne - she does it to make a home for him - home sweet home - the bubu for breakfast - nose rash so i had to switch it up - news flash when did you become a judge? - Bright & early - birds chirping - singing to me to get the word in - on the balcony - a cup of coffee - call eddie - talk about the new beat - if i like the beat then ima use it - give it all away so you cant sue me - i worked the past 10 years for free - not hiding - you know where to find me - home sweet home - the bubu for breakfast - nose rash so i had to switch it up - news flash - when did you become a judge ? - are you lord da bubu ? da bubu - home sweet home x2",
+  },
+  {
+    title: "C.O.R.E. (feat. Dòsh Dósh)",
+    lyrics:
+      "We are back from the dead like Lazarus - for better or worse a road awaits up - drive through with the legs like drills - we wear only black like johnny cash to symbolize the downtrodden - pros at rejection and self-hatred - is it real or is it fake ? - it sure seems real - convinced that its real now - that wasn't the deal - we escape by pooring out our inner beings - on the canvas - on the song - on the paper - on the skin - poor it out now from the core of your being x8 - poor it out now - poor it out now from the core of your being x4 ",
+  },
+  {
+    title: "Opposite Spirit (feat. Dòsh Dósh)",
+    lyrics:
+      "Yahwehs a creator - made in his image so we are creators - devil im not impressed - with God however - dumbfounded - horses and chariotts of fire - don't shoot the horse - shoot the rider - bows and arrows dipped in fire - ready - aim - fire - there's passion in the blood pulsing through our veins - in the opposite spirit is where we stay - as it is written - i am the head and not the tail - above and not beneath - set a guard oh Lord over my mouth - keep watch over the door of my lips - David's prayer is our prayer now too - let the weak say that i'm strong and be strong - there's passionn in the blood pulsing through our veins - in the opposite spirit is where we stay",
+  },
+  {
+    title: "Peace, Blood, Water",
+    lyrics:
+      "From the dust we come - to the dust we go x2 - from the dust (three fours) to the dust (three twos) from the dust (two eights) to the dust (ninety six) - they say blood is thicker then water - i say peace is thicker then blood - my family is not my family now - she's my family now x2 - From the dust we come - to the dust we go x2 - from the dust (three fours) to the dust (three twos) from the dust (two eights) to the dust (ninety six) - they say blood is thicker then water - i say peace is thicker then blood ",
+  },
+  {
+    title: "Rarity",
+    lyrics: "",
+  },
+  {
+    title: "Jersey Boy",
+    lyrics: "",
+  },
+  {
+    title: "Folded Flags (feat. Robert Dougherty & Dòsh Dósh)",
+    lyrics: "",
+  },
+];
