d2af1ce5c9f6aadd04c1cf4e366ba7b196b22f1e
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-03-08 16:13:38 -0800

    feat: new array keys and p tags and h2 tags

--- scripts/script.js
+++ scripts/script.js
@@ -3,38 +3,52 @@
 const highlights = [
   {
     song: "Look Up",
+    year: "2024",
     link: "https://www.youtube.com/embed/zbF3SsoTUvA?si=is5eNOwXuqZPKRKu",
   },
   {
     song: "A Lil' Fancy",
+    year: "2024",
     link: "https://www.youtube.com/embed/iPqlSTPTNC8?si=xnzyXWf0mJZu5Vay",
   },
   {
     song: "Kahina",
+    year: "2019",
     link: "https://www.youtube.com/embed/A0dovpf779A?si=uVPKRurhaxLZHkEe",
   },
   {
     song: "Erth",
+    year: "2021",
     link: "https://www.youtube.com/embed/ZulWAyu0OlQ?si=-b7Xpdv1QamSfuoE",
   },
   {
     song: "Mercury",
+    year: "2021",
     link: "https://www.youtube.com/embed/h7zJg8XM7Fs?si=kOjgmIZbLb9qIkkn",
   },
   {
     song: "New World",
+    year: "2021",
     link: "https://www.youtube.com/embed/csjlAccd3aQ?si=JNixjpBn2WXMxkck",
   },
   {
+    song: "Sayonara!",
+    year: "2021",
+    link: "#",
+  },
+  {
     song: "All She Wrote",
+    year: "2022",
     link: "https://www.youtube.com/embed/wahvaciH9Xg?si=OSRlq4IX13pio3fE",
   },
   {
     song: "Save The Children",
+    year: "2024",
     link: "https://www.youtube.com/embed/S9YGRYt6E34?si=9-KZA2lEEQR-3j8m",
   },
   {
     song: "C.O.R.E.",
+    year: "2025",
     link: "#",
   },
 ];
@@ -43,53 +57,63 @@ const highlights = [
 
 const discography = [
   {
-    record: "Octave .0",
+    title: "Octave .0",
     type: "Album",
+    year: "2024",
     link: "https://open.spotify.com/embed/album/1j8F0KOLxKiKPHQMcBxPf1?utm_source=generator&theme=0",
   },
   {
-    record: "Look Up",
+    title: "Look Up",
     type: "Single",
+    year: "2024",
     link: "https://open.spotify.com/embed/album/5rUnTmZTv7VWrY64Ofofy7?utm_source=generator&theme=0",
   },
   {
-    record: "Filthy Rags",
+    title: "Filthy Rags",
     type: "Single",
+    year: "2024",
     link: "https://open.spotify.com/embed/album/3bPW2pKTVc2U1thtpv07Mv?utm_source=generator&theme=0",
   },
   {
-    record: "SEED-3000",
+    title: "SEED-3000",
     type: "Album",
+    year: "2024",
     link: "https://open.spotify.com/embed/album/3hYcn73DOge96MiPc1PFKW?utm_source=generator&theme=0",
   },
   {
-    record: "A Lil' Fancy",
+    title: "A Lil' Fancy",
     type: "Single",
+    year: "2024",
     link: "https://open.spotify.com/embed/album/3Irm7unt1ZNJAXNuHqKpSz?utm_source=generator&theme=0",
   },
   {
-    record: "L'OWL'Y",
+    title: "L'OWL'Y",
     type: "E.P.",
+    year: "2023",
     link: "https://open.spotify.com/embed/album/3iGx0LC84dpNiWC9ogkezp?utm_source=generator&theme=0",
   },
   {
-    record: "REALM IV",
+    title: "REALM IV",
     type: "E.P.",
+    year: "2022",
     link: "https://open.spotify.com/embed/album/553ZbYATN2ZZeyUhVE7q5o?utm_source=generator&theme=0",
   },
   {
-    record: "S-O-L",
+    title: "S-O-L",
     type: "E.P.",
+    year: "2022",
     link: "https://open.spotify.com/embed/album/7Bm3ub7N78GKzdH05w7jlW?utm_source=generator&theme=0",
   },
   {
-    record: "III DOOR",
+    title: "III DOOR",
     type: "Album",
+    year: "2021",
     link: "https://open.spotify.com/embed/album/2sXPrZBVJwHbCjc3R87FCP?utm_source=generator&theme=0",
   },
   {
-    record: "T-PO'AK",
+    title: "T-PO'AK",
     type: "Album",
+    year: "2021",
     link: "https://open.spotify.com/embed/album/17KtMScOLI3Ic8fIXUtksL?utm_source=generator&theme=0",
   },
 ];
