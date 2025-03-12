12bbdcbaa39f5c65cc866791f3e16302310348e2
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-03-12 12:31:14 -0700

    lyric pics added

--- scripts/lyrics.js
+++ scripts/lyrics.js
@@ -1,27 +1,33 @@
 const Octave0Lyrics = [
   {
     songTitle: "Da Bubu",
-    lyricsPic: "",
+    lyricsPic:
+      "https://drive.google.com/file/d/1_K3-CS5WdzhcRCRLDy34rmJvlykqgHhy/view?usp=drive_link",
   },
   {
     songTitle: "C.O.R.E. (feat. Dòsh Dósh)",
-    lyricsPic: "",
+    lyricsPic:
+      "https://drive.google.com/file/d/1Bi15EXOCKi4zMDn0jBIZLYh7RCTW3lYd/view?usp=drive_link",
   },
   {
     songTitle: "Opposite Spirit (feat. Dòsh Dósh)",
-    lyricsPic: "",
+    lyricsPic:
+      "https://drive.google.com/file/d/1iq3nWJAGCWEg3K3TiqJpq48u0SvgfiTh/view?usp=drive_link",
   },
   {
     songTitle: "Peace, Blood, Water",
-    lyricsPic: "",
+    lyricsPic:
+      "https://drive.google.com/file/d/1ax2O4NAA_C96Pqr4QNw-FuMytZ3ekVr8/view?usp=drive_link",
   },
   {
     songTitle: "Rarity",
-    lyricsPic: "",
+    lyricsPic:
+      "https://drive.google.com/file/d/1ZVYPw2MOrp1GZYh4xKCymHqK3yhkpk66/view?usp=drive_link",
   },
   {
     songTitle: "Jersey Boy",
-    lyricsPic: "",
+    lyricsPic:
+      "https://drive.google.com/file/d/1EwT2CfuNW05MNJcJM3zdbOYIrCieC80m/view?usp=drive_link",
   },
   {
     songTitle: "Folded Flags (feat. Robert Dougherty & Dòsh Dósh)",
